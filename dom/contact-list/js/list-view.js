87b8fd691ef8462485d26f291f30411c23c7bed6
Ilya <rodionov84@mail.ru>
2018-08-06 19:26:17 +0300

    06-08-2018

--- dom/contact-list/js/list-view.js
+++ dom/contact-list/js/list-view.js
@@ -1,4 +1,5 @@
 let container;
+let contactsList;
 
 function loadContacts() {
   return '[{"name":"Василий Николаев","email":"vnikola9999@gmail.com","phone":"+7 999 777 34 34"},{"name":"Елена Вишневская","email":"lenochka22333@yandex.ru","phone":"+7 888 777 11 11"},{"name":"Артём Кузнецов","email":"kuznya_foreva@gmail.com","phone":"+7 222 555 76 67"},{"name":"Алексей Гусенко","email":"jiznboliyaetoznayu@mail.com","phone":"+7 333 545 12 34"},{"name":"Маргарита Сотникова","email":"pobeditelnicapojizni111@gmail.com","phone":"+7 323 534 32 12"}]';
@@ -31,8 +32,28 @@ function backClick() {
 
 function init() {
   container = document.getElementById('container');
-  container.querySelector('.list-view').addEventListener('click', contactClick);
   container.querySelector('.back').addEventListener('click', backClick);
+  contactsList = document.getElementsByClassName('contacts-list')[0];
+
+  const contacts = JSON.parse(loadContacts());
+  setContactsList(contacts);
+}
+
+function setContactsList(contacts) {
+  contactsList.innerHTML = '';
+
+  contacts.forEach((contact) => {
+    let strong = document.createElement('strong');
+    strong.innerHTML = contact.name;
+
+    let li = document.createElement('li');
+    li.setAttribute('data-email', contact.email);
+    li.setAttribute('data-phone', contact.phone);
+    li.appendChild(strong);
+    li.addEventListener('click', contactClick);
+
+    contactsList.appendChild(li);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
--- dom/shop/js/price-format.js
+++ dom/shop/js/price-format.js
@@ -1,3 +1,38 @@
+let container;
+let cartCount;
+let cartTotalPrice;
+let currentCartCount;
+let currentCartTotalPrice;
+
 function getPriceFormatted(value) {
   return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+function init() {
+  container = document.getElementById('container');
+
+  cartCount = document.getElementById('cart-count');
+  cartTotalPrice = document.getElementById('cart-total-price');
+
+  currentCartCount = 0;
+  currentCartTotalPrice = 0;
+
+  const addButtons = container.querySelectorAll('.add');
+  addButtons.forEach((button) => {
+  	button.addEventListener('click', addToCart)
+  });
+}
+
+function addToCart(event) {
+  event.preventDefault();
+
+  const price = parseInt(event.currentTarget.dataset.price);
+
+  currentCartCount++;
+  currentCartTotalPrice += price;
+
+  cartCount.innerHTML = currentCartCount;
+  cartTotalPrice.innerHTML = getPriceFormatted(currentCartTotalPrice);
+}
+
+document.addEventListener('DOMContentLoaded', init);
