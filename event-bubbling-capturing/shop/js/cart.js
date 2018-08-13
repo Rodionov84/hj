d70e13ad7349236e715a056b5972a7a374e81457
Ilya <rodionov84@mail.ru>
2018-08-13 16:36:41 +0300

    13-08-2018

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const itemsList = document.getElementsByClassName('items-list')[0];
+itemsList.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  const button = event.target;
+  if (!button.hasAttribute('data-title') || 
+  	  !button.hasAttribute('data-price') || 
+  	  !button.classList.contains('add-to-cart')) {
+  	return;
+  }
+
+  event.preventDefault();
+
+  const item = {
+  	title: button.getAttribute('data-title'),
+  	price: button.getAttribute('data-price')
+  };
+
+  addToCart(item);
+}
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+	const th = event.target;
+	if( !th.hasAttribute('data-prop-name') || th.nodeName !== 'TH' ) {
+	  return;
+	}
+
+	let dir;
+	if( !th.hasAttribute('data-dir') ) {
+	  th.setAttribute('data-dir', '1');
+	  dir = 1;
+	}
+	else {
+	  dir = th.getAttribute('data-dir') === '1' ? -1 : 1;
+	  th.setAttribute('data-dir', dir);
+	}
+
+	let sortBy = th.getAttribute('data-prop-name');
+	this.setAttribute('data-sort-by', sortBy);
+
+	sortTable(sortBy, dir);
 }
